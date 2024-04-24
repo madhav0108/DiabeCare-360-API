6179d285147fa55fb16ce11523d9ad3749c82e4e
madhav <madhav.sharma@abbott.com>
2024-04-24 14:02:43 -0700

    environment variables set

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://msharm51:Madhav94Aishwa8010@diabecare360.ivi7fpk.mongodb.net/";  // Your MongoDB Atlas connection string
+const uri = process.env.MONGODB_URI;  // Use environment variable
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connect() {
