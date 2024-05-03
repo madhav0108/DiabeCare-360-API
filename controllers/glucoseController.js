516a160ea1273e512724f9d5ab71c1dd763b339d
madhav <madhav.sharma@abbott.com>
2024-05-03 14:52:42 -0700

    Handling Date in Backend to fit Swift

--- controllers/glucoseController.js
+++ controllers/glucoseController.js
@@ -18,6 +18,10 @@ exports.createGlucose = async (req, res) => {
 exports.getAllGlucose = async (req, res) => {
     try {
         const glucoseData = await Glucose.find();
+        const formattedData = glucoseData.map(g => ({
+            level: g.level,
+            date: g.date.toISOString()  // Ensure date is in ISO 8601 string format
+        }));
         res.status(200).json(glucoseData); // This will always be an array, even if it's empty
     } catch (error) {
         res.status(400).json({ message: "Failed to get glucose data", error: error.message });
