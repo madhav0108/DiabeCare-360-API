dfe6d5c7595a3a689890deab0a8acffb2b9e1a69
madhav <madhav.sharma@abbott.com>
2024-05-13 19:33:30 -0700

    Fix module path -2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const authenticateToken = require('./middleware/authenticationToken');
+const authenticateToken = require('../middleware/authenticationToken');
 
 // User registration
 router.post('/register', userController.registerUser);
