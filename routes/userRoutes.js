ac8244c0078cd7639d31a312b19c629b81189dfa
madhav <madhav.sharma@abbott.com>
2024-05-21 15:38:06 -0700

    Check If User Exists --X

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,9 +15,6 @@ router.post('/logout', authenticateToken, userController.logoutUser);
 // User details
 router.get('/userDetails', authenticateToken, userController.getUserDetails);
 
-// Check if user exists
-router.post('/check-user-exists', userController.checkIfUserExists);
-
 // Password reset request
 router.post('/reset-password-request', userController.resetPasswordRequest);
 
