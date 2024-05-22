2bc07f8871861f74489c7464932ad176d7e582b5
madhav <madhav.sharma@abbott.com>
2024-05-22 12:15:20 -0700

    Import Schema fix

--- models/Glucose.js
+++ models/Glucose.js
@@ -1,10 +1,12 @@
 // models/Glucose.js
 const mongoose = require('mongoose');
+const { Schema } = mongoose; // Import Schema
 
-const glucoseSchema = new mongoose.Schema({
+const glucoseSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },  // Reference to the User model
     level: { type: Number, required: true },
     date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Glucose', glucoseSchema, 'glucose');
+const Glucose = mongoose.model('Glucose', glucoseSchema, 'glucose'); // Explicitly set collection name
+module.exports = Glucose;
